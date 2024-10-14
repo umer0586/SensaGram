039ad6dab6f70547bb95a1a69ffb1b681644b2c7
umer0586 <umerfarooq.phone@gmail.com>
2024-10-14 14:57:58 +0500

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.github.umer0586.sensagram"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
