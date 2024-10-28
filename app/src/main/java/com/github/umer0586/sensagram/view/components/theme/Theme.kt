524872883831da9116e05cb219daa700ea389712
umer0586 <umerfarooq.phone@gmail.com>
2024-10-28 21:45:48 +0500

    Unused import removed

--- app/src/main/java/com/github/umer0586/sensagram/view/components/theme/Theme.kt
+++ app/src/main/java/com/github/umer0586/sensagram/view/components/theme/Theme.kt
@@ -1,10 +1,10 @@
 package com.github.umer0586.sensagram.view.components.theme
 import android.os.Build
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
